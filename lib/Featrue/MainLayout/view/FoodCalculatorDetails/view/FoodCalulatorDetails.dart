2aebdd08e0fa3e1ea34ed7058c1b365a723c3a36
Samuel Magdy <116669998+samuelmagdy555@users.noreply.github.com>
2024-10-17 04:43:03 +0300

    //FirstCommit

--- lib/Featrue/MainLayout/view/FoodCalculatorDetails/view/FoodCalulatorDetails.dart
+++ lib/Featrue/MainLayout/view/FoodCalculatorDetails/view/FoodCalulatorDetails.dart
@@ -28,87 +28,78 @@ class _FoodCalculatorDetailsState extends State<FoodCalculatorDetails> {
         title: const Text('Food Calculator Details'),
         backgroundColor: Colors.teal,
       ),
-      body:
-          BlocConsumer<FoodCalculatorDetailsCubit, FoodCalculatorDetailsState>(
-              listener: (context, state) {
-        if (state is FoodCalculatorDetailsError) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Error loading details')),
-          );
-        }
-      }, builder: (context, state) {
-        final calculationData = FoodCalculatorDetailsCubit.get(context)
-            .foodCalculatorDetails!
-            .data
-            .calculationData;
+      body: BlocConsumer<FoodCalculatorDetailsCubit, FoodCalculatorDetailsState>(
+        listener: (context, state) {
+          if (state is FoodCalculatorDetailsError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Error loading details')),
+            );
+          }
+        },
+        builder: (context, state) {
 
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Enter the quantity in grams:',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  controller: _quantityController,
-                  keyboardType: TextInputType.number,
-                  cursorColor: Colors.white,
-                  style: TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
-                    hintStyle: TextStyle(color: Colors.white),
-                    labelText: 'Quantity (grams)',
-                    labelStyle: TextStyle(color: Colors.white),
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a quantity';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      context
-                          .read<FoodCalculatorDetailsCubit>()
-                          .fetchFoodCalculatorDetails(
+            final calculationData = FoodCalculatorDetailsCubit.get(context).foodCalculatorDetails!.data.calculationData;
+
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Enter the quantity in grams:',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _quantityController,
+                      keyboardType: TextInputType.number,
+                      cursorColor: Colors.white,
+style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        hintStyle: TextStyle(color: Colors.white),
+                        labelText: 'Quantity (grams)',
+                        labelStyle: TextStyle(color: Colors.white),
+
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a quantity';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          context.read<FoodCalculatorDetailsCubit>().fetchFoodCalculatorDetails(
                             mealId: widget.id,
                             quantities: _quantityController.text,
                           );
-                    }
-                  },
-                  child: const Text('Calculate'),
+                        }
+                      },
+                      child: const Text('Calculate'),
+                    ),
+                    const SizedBox(height: 20),
+                    if (calculationData != null) ...[
+                      _buildNutrientRow('Proteins', calculationData.protein , ),
+                      _buildNutrientRow('Calories', calculationData.calories),
+                      _buildNutrientRow('Carbohydrates', calculationData.carbs),
+                      _buildNutrientRow('Fats', calculationData.fats),
+                      _buildNutrientRow('Fibers', calculationData.fibers),
+                    ] else ...[
+                      const Center(child: Text('No Data Available', style: TextStyle(color: Colors.black))),
+                    ],
+                  ],
                 ),
-                const SizedBox(height: 20),
-                if (calculationData != null) ...[
-                  _buildNutrientRow(
-                    'Proteins',
-                    calculationData.protein,
-                  ),
-                  _buildNutrientRow('Calories', calculationData.calories),
-                  _buildNutrientRow('Carbohydrates', calculationData.carbs),
-                  _buildNutrientRow('Fats', calculationData.fats),
-                  _buildNutrientRow('Fibers', calculationData.fibers),
-                ] else ...[
-                  const Center(
-                      child: Text('No Data Available',
-                          style: TextStyle(color: Colors.black))),
-                ],
-              ],
-            ),
-          ),
-        );
-      }),
+              ),
+            );
+          }
+
+      ),
     );
   }
 
@@ -120,13 +111,11 @@ class _FoodCalculatorDetailsState extends State<FoodCalculatorDetails> {
         children: [
           Text(
             '$label:',
-            style: const TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white),
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600,color: Colors.white),
           ),
           Text(
             '$value g',
-            style: const TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold , color: Colors.white),
           ),
         ],
       ),
