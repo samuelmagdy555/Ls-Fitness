5b308ffbdf17d54f48d0b1d20d818f45f403c724
kero harby <bolly333333@gmail.com>
2025-01-20 06:03:14 +0200

    handel change Message ID

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,10 +9,10 @@ import alarm
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-     if #available(iOS 10.0, *) {
-         UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-     }
-     SwiftAlarmPlugin.registerBackGroundTask()
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    SwiftAlarmPlugin.registerBackgroundTasks()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
